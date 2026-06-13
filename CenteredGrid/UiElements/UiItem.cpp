0c3d8411765f41c3e2bbeac8aeb9e88790beb4d4
Yolked64 <leobodinleo@gmail.com>
2026-06-13 21:00:38 +0200

    Updated Ui Item to fit more the kind of use it will have Ie no sprites

--- CenteredGrid/UiElements/UiItem.cpp
+++ CenteredGrid/UiElements/UiItem.cpp
@@ -10,7 +10,7 @@ bool UiItem::IsMouseHovering(Vector2 MousePosition) const
 	return CheckCollisionPointRec(MousePosition, this->Area);
 }
 
-void UiItem::Draw(const Texture& Atlas) const
+void UiItem::Draw() const
 {
-	DrawTexturePro(Atlas, this->AtlasArea, this->Area, this->SpriteOrigin, this->SpriteRotation, WHITE);
+	DrawRectangleRec(this->Area, BackgroundColor);
 }
--- CenteredGrid/UiElements/UiItem.hpp
+++ CenteredGrid/UiElements/UiItem.hpp
@@ -10,12 +10,11 @@ public:
 
 	bool IsMouseHovering(Vector2 MousePosition) const;
 	Rectangle GetArea() const;
-	virtual void Draw(const Texture& Atlas) const;
+	virtual void Draw() const;
 	virtual void Update() = 0;
 protected:
 	Rectangle Area = Rectangle(0, 0, 0, 0);
-	Rectangle AtlasArea = Rectangle(0, 0, 0, 0);
-
-	Vector2 SpriteOrigin = Vector2(0, 0);
-	float SpriteRotation = 0.0f;
+	
+	Color BackgroundColor = BLACK;
+	int BorderWidth = 0;
 };
